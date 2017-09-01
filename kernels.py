c4dfcd9159e343236f895dd6646dac4f45e08bd5
qingshan412 <qingshan412@hotmail.com>
2017-08-31 23:01:13 -0400

    same as eval_gen

--- kernels.py
+++ kernels.py
@@ -13,7 +13,15 @@ class ParsenDensityEstimator(object):
         :param sigma: variance
         :return:
         """
-        d = (tf.expand_dims(x, 0) - mu) / sigma
-        e = -0.5 * tf.reduce_sum(tf.multiply(d, d), axis=2)
-        z = tf.to_float(tf.shape(mu)[2]) * tf.log(np.float32(sigma * np.sqrt(np.pi * 2.0)))
+        # J.L. 
+        #d = (tf.expand_dims(x, 0) - mu) / sigma
+        #e = -0.5 * tf.reduce_sum(tf.multiply(d, d), axis=2)
+        #z = tf.to_float(tf.shape(mu)[2]) * tf.log(np.float32(sigma * np.sqrt(np.pi * 2.0)))
+        d = (tf.expand_dims(x, 0) - mu) 
+        e = -0.5 * tf.reduce_sum(tf.multiply(d, d) / sigma, axis=2)
+        z = tf.to_float(tf.shape(mu)[2]) * tf.log(np.float32(np.sqrt(sigma * np.pi * 2.0)))
         return e - z
+
+#-T.sum(T.square(generated-T.addbroadcast(self.obs,0)),[-1,-2]) / (2*self.sigma)
+#- k/2.*np.log(2 * np.pi)
+#- k/2.*np.log(self.sigma)
