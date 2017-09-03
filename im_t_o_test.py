790ec76d7c8fff741c840b90039eafd3d643f457
qingshan412 <qingshan412@hotmail.com>
2017-09-03 12:23:04 -0400

    restore

--- im_t_o_test.py
+++ im_t_o_test.py
@@ -41,7 +41,7 @@ def conv_out_size_same(size, stride):
   return int(math.ceil(float(size) / float(stride)))
 
 class Generator(object):
-    def __init__(self, sess#input_height=32, input_width=32, 
+    def __init__(self, sess, #input_height=32, input_width=32, 
          crop=True,
          batch_size=64, sample_num = 64, output_height=32, output_width=32,
          y_dim=None, z_dim=100, gf_dim=128, df_dim=128,
@@ -153,9 +153,9 @@ class Generator(object):
         self.genImage = tf.nn.tanh(h6)
 
     def __call__(self, z):
-	with tf.Session() as sess:
-	    tmp_z = tf.Session().run(z)
-	    sess.close()
+	    with tf.Session() as sess:
+	        tmp_z = tf.Session().run(z)
+	        sess.close()
         #sess.run(self.assign(z))
         return self.sess.run(self.genImage, feed_dic={self.z:tmp_z})
         #return tf.nn.tanh(h6)
