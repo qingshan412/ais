05aa0e072b14421949794d2afd7a5d77fa8fb0ed
qingshan412 <qingshan412@hotmail.com>
2017-09-02 23:40:32 -0400

    restore

--- im_test_origin_testImage.py
+++ im_test_origin_testImage.py
@@ -7,8 +7,8 @@ from kernels import ParsenDensityEstimator
 from scipy.stats import norm
 
 #import re
-#from ops_dcgan import *
-from ops import *
+#from ops import *
+#from utils import *
 import math
 
 import argparse
@@ -79,79 +79,91 @@ class Generator(object):
         self.g_bn5 = batch_norm(name='g_bn5')
 
         self.dataset_name = dataset_name
-        self.z = tf.placeholder(tf.float32, [1, self.z_dim], name='z')#J.L.
-        
-        #could_load, checkpoint_counter = self.load(self.checkpoint_dir)
-        #with tf.variable_scope(tf.get_variable_scope()) as scope:
-        with tf.variable_scope("generator") as scope:
-            #scope.reuse_variables()
-            s_h, s_w = self.output_height, self.output_width
-            print(s_h)
-            s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
-            print(s_h2)
-            s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
-            print(s_h4)
-            s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
-            print(s_h8)
-
-            # project `z` and reshape #J.L.
-            self.z_, self.h0_w, self.h0_b = linear(self.z, self.gf_dim*4*s_h8*s_w8, 'g_h0_lin', with_w=True)
-            self.h0 = tf.nn.relu(self.g_bn0(self.z_))
-            print('h:')
-
-            h0 = tf.reshape(self.h0, [-1, s_h8, s_w8, self.gf_dim * 4])
-            print(h0.get_shape().as_list())
-            
-            self.h1, self.h1_w, self.h1_b = deconv2d_d2(h0, [self.batch_size, s_h4, s_w4, self.gf_dim*4],name='g_h1', with_w=True)
-            h1 = tf.nn.relu(self.g_bn1(self.h1))
-            print(h1.get_shape().as_list())
-
-            h2, self.h2_w, self.h2_b = deconv2d_d1(h1, [self.batch_size, s_h4, s_w4, self.gf_dim*2], name='g_h2', with_w=True)
-            h2 = tf.nn.relu(self.g_bn2(h2))
-            print(h2.get_shape().as_list())
-
-            h3, self.h3_w, self.h3_b = deconv2d_d2(h2, [self.batch_size, s_h2, s_w2, self.gf_dim*2], name='g_h3', with_w=True)
-            h3 = tf.nn.relu(self.g_bn3(h3))
-            print(h3.get_shape().as_list())
-
-            h4, self.h4_w, self.h4_b = deconv2d_d1(h3, [self.batch_size, s_h2, s_w2, self.gf_dim],name='g_h4', with_w=True)
-            h4 = tf.nn.relu(self.g_bn4(h4))
-            print(h4.get_shape().as_list())
-
-            h5, self.h5_w, self.h5_b = deconv2d_d2(h4, [self.batch_size, s_h, s_w, self.gf_dim], name='g_h5', with_w=True)
-            h5 = tf.nn.relu(self.g_bn5(h5))
-            print(h5.get_shape().as_list())
-
-            h6, self.h6_w, self.h6_b = deconv2d_d1(h5, [self.batch_size, s_h, s_w, self.c_dim], name='g_h6', with_w=True)
-            print(h6.get_shape().as_list())
-
+        self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
         self.saver = tf.train.Saver()
 
         #def load(self):
-        #v1 = tf.get_variable("v") 
-        #tf.global_variables_initializer()#.run()
-#        print(tf.get_variable("generator/g_bn0/beta", [8192]).name)
-        #generator/g_bn0/beta"
         print(" [*] Reading checkpoints...")
         #checkpoint_dir = os.path.join(self.checkpoint_dir, self.model_dir)
         ckpt = tf.train.get_checkpoint_state(self.checkpoint_dir)
         if ckpt and ckpt.model_checkpoint_path:
             ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
-            print(ckpt_name)
             self.saver.restore(self.sess, os.path.join(self.checkpoint_dir, ckpt_name))
             print(" [*] Success to read {}".format(ckpt_name))
             #return True
         else:
             print(" [*] Failed to find a checkpoint")
             exit(0)#return False
-        
-        self.genImage = tf.nn.tanh(h6)
+        #could_load, checkpoint_counter = self.load(self.checkpoint_dir)
+        with tf.variable_scope("generator") as scope:
+            scope.reuse_variables()
+            s_h, s_w = self.output_height, self.output_width
+            s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
+            s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
+            s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
+
+            # project `z` and reshape
+            self.z_, self.h0_w, self.h0_b = linear(z, self.gf_dim*4*s_h8*s_w8, 'g_h0_lin', with_w=True)
+            self.h0 = tf.nn.relu(self.g_bn0(self.z_))
+
+            h0 = tf.reshape(self.h0, [-1, s_h8, s_w8, self.gf_dim * 4])
+            
+            self.h1, self.h1_w, self.h1_b = deconv2d_d2(h0, [self.batch_size, s_h4, s_w4, self.gf_dim*4],name='g_h1', with_w=True)
+            h1 = tf.nn.relu(self.g_bn1(self.h1))
+
+            h2, self.h2_w, self.h2_b = deconv2d_d1(h1, [self.batch_size, s_h4, s_w4, self.gf_dim*2], name='g_h2', with_w=True)
+            h2 = tf.nn.relu(self.g_bn2(h2))
+
+            h3, self.h3_w, self.h3_b = deconv2d_d2(h2, [self.batch_size, s_h2, s_w2, self.gf_dim*2], name='g_h3', with_w=True)
+            h3 = tf.nn.relu(self.g_bn3(h3))
+
+            h4, self.h4_w, self.h4_b = deconv2d_d1(h3, [self.batch_size, s_h2, s_w2, self.gf_dim],name='g_h4', with_w=True)
+            h4 = tf.nn.relu(self.g_bn4(h4))
+
+            h5, self.h5_w, self.h5_b = deconv2d_d2(h4, [self.batch_size, s_h, s_w, self.gf_dim], name='g_h5', with_w=True)
+            h5 = tf.nn.relu(self.g_bn5(h5))
+
+            h6, self.h6_w, self.h6_b = deconv2d_d1(h5, [self.batch_size, s_h, s_w, self.c_dim], name='g_h6', with_w=True)
 
     def __call__(self, z):
-        return(self.sess.run(self.genImage, feed_dict={self.z: z}))
-        #return tf.nn.tanh(h6)
+            return tf.nn.tanh(h6)
 
 
-generator = Generator(checkpoint_dir=checkpoint_dir, dataset_name='default')
-print('load success!')
-exit(0)
+
+
+
+PicPath = ''
+PicFiles = [ f for f in listdir(PicPath) if path.isfile(path.join(PicPath,f)) and f.strip().split('.')[-1]=='jpg']#
+countflag = 0
+for Pic in PicFiles:
+    tmp = Image.open(path.join(PicPath, Pic))
+    print(countflag)
+    line3 = np.array(tmp)
+    line3 = line3.flatten()
+    if countflag == 0:
+        AllPx = line6
+    else:
+        AllPx = np.vstack((AllPx, line6))
+
+    countflag = countflag + 1
+    if countflag >= NumBatch:
+        break
+
+xx = AllPx
+# Get 100 f(x) values
+#p = norm()
+#x = np.linspace(norm.ppf(0.01, loc=3, scale=2), norm.ppf(0.99, loc=3, scale=2), 100)
+#p1 = norm.pdf(x, loc=3, scale=2)
+#xx = np.reshape(x, (100, 1))
+
+schedule = ais.get_schedule(100, rad=4)
+print(schedule)
+p2 = np.exp(model.ais(xx, schedule))
+print(p2)
+
+lld = model.ais(xx, schedule)
+print('lld_mean:')
+print(np.mean(lld))
+plt.plot(x, p1)
+plt.plot(x, p2)
+plt.show()
