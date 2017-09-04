d54eaaf58227ccfda2c335f1606dce2eb5b0e512
qingshan412 <qingshan412@hotmail.com>
2017-09-03 23:18:55 -0400

    il

--- im_testImage.py
+++ im_testImage.py
@@ -10,7 +10,6 @@ from scipy.stats import norm
 #from ops_dcgan import *
 from ops import *
 import math
-import pickle
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -153,59 +152,8 @@ class Generator(object):
 #kernel = ParsenDensityEstimator()
 #model = ais.Model(generator, prior, kernel, 0.25, 10000)
 
-
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo)
-    return dict
-
-def load_databatch(data_folder, idx, img_size=32, train='train'):
-
-    if train != 'train':
-        data_file = os.path.join(data_folder, 'val_data')
-    else:
-        data_file = os.path.join(data_folder, 'train_data_batch_')
-    print(data_file)
-    #
-
-    d = unpickle(data_file + str(idx))
-    x = d['data']
-    #y = d['labels']
-    #mean_image = d['mean']
-
-    x = x/np.float32(256)#255
-    #mean_image = mean_image/np.float32(255)
-
-    # Labels are indexed from 1, shift it so that indexes start at 0
-    #y = [i-1 for i in y]
-    data_size = x.shape[0]
-
-    #x -= mean_image
-
-    img_size2 = img_size * img_size
-
-    x = np.dstack((x[:, :img_size2], x[:, img_size2:2*img_size2], x[:, 2*img_size2:]))
-    x = x.reshape((x.shape[0], img_size, img_size, 3)).transpose(0, 3, 1, 2)
-
-    # create mirrored images
-    #X_train = x[0:data_size, :, :, :]
-    #Y_train = y[0:data_size]
-    #X_train_flip = X_train[:, :, :, ::-1]
-    #Y_train_flip = Y_train
-    #X_train = np.concatenate((X_train, X_train_flip), axis=0)
-    #Y_train = np.concatenate((Y_train, Y_train_flip), axis=0)
-
-    Px = np.sum(x,axis=1)
-    line1 = Px.reshape((data_size,-1))
-    line2 = line1 + np.random.random(line1.shape)/256.0 #add noise
-
-    return line2#dict(
-        #X_train=lasagne.utils.floatX(X_train),
-        #Y_train=Y_train.astype('int32'),
-        #mean=mean_image)
-
-PicPath = '../../DataImageNet/Image32/train32'
-AllPx = load_databatch(data_folder = PicPath, idx = 1)
+PicPath = '../../DataImageNet/Image32/valid32/AllPx_valid.npy'
+AllPx = np.load(data_folder = PicPath)
 # train32
 # for i in xrange(9):
 #    tmpPx = load_databatch(data_folder = PicPath, idx = i+2)
