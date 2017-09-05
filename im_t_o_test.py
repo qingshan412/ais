ea6b75e43e332f7d6271a03284f8d4fd05b843ea
qingshan412 <qingshan412@hotmail.com>
2017-09-04 22:56:29 -0400

    gen

--- im_t_o_test.py
+++ im_t_o_test.py
@@ -37,45 +37,27 @@ checkpoint_dir = args.CheckP
 #kernel = ParsenDensityEstimator()
 #model = ais.Model(generator, prior, kernel, 0.25, 10000)
 
-def conv_out_size_same(size, stride):
-  return int(math.ceil(float(size) / float(stride)))
-
 class Generator(object):
-    def __init__(self, #sess, #input_height=32, input_width=32, 
-         crop=True,
+    def __init__(self, #sess, #input_height=32, input_width=32, crop=True,
          batch_size=64, sample_num = 64, output_height=32, output_width=32,
          y_dim=None, z_dim=100, gf_dim=128, df_dim=128,
          gfc_dim=1024, dfc_dim=1024, c_dim=3,checkpoint_dir=None, dataset_name='default'):
         
-        self.output_dim = 3*output_height*output_width
         self.input_dim = z_dim
-        self.sess = tf.Session()
-        self.checkpoint_dir=checkpoint_dir
-
-        print(" [*] Reading checkpoints...")
-        #checkpoint_dir = os.path.join(self.checkpoint_dir, self.model_dir)
-        ckpt = tf.train.get_checkpoint_state(self.checkpoint_dir)
-        if ckpt and ckpt.model_checkpoint_path:
-            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
-            print(ckpt_name)
-            new_saver = tf.train.import_meta_graph(os.path.join(self.checkpoint_dir, ckpt_name + '.meta'))
-            new_saver.restore(self.sess, os.path.join(self.checkpoint_dir, ckpt_name))
-            #self.saver.restore(self.sess, os.path.join(self.checkpoint_dir, ckpt_name + '.data-00000-of-00001'))
-            print(" [*] Success to read {}".format(ckpt_name))
-            #return True
-        else:
-            print(" [*] Failed to find a checkpoint")
-            exit(0)#return False
-        #self.sess.close()
-        self.dcgan.ais = dcgan.ais
-        self.dcgan.z = dcgan.z
+        self.output_dim = 3*output_height*output_width
+        self.sess = sess
+        new_saver = tf.train.import_meta_graph('my-save-dir/my-model-10000.meta')
+        new_saver.restore(sess, 'my-save-dir/my-model-10000')
+        #saver.restore(sess, "/tmp/model.ckpt")
+        print("Model restored.")
+        self.v3h = tf.get_collection("v3")[0]
+        self.a23th = tf.get_collection("tfadd")[0]
 
     def __call__(self, z):
-        return self.sess.run(self.dcgan.ais,feed_dict={self.dcgan.z: z})
-        #return tf.nn.tanh(h6)
+        return self.sess.run(self.a23th, feed_dict={self.v3h:z})
 
 #with tf.Session() as sess:
-generator = Generator(sample_num=NumSample, checkpoint_dir=checkpoint_dir)
+generator = Generator(tf.Session(), sample_num=NumSample)
 print('init success!')
 z = np.random.normal(0.0, 1.0, [64 * NumSample, 100])
 ct = generator(z)
