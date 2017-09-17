f2123e0c835cc9932a8e457025d3894acfa583ac
qingshan412 <qingshan412@hotmail.com>
2017-09-17 17:05:25 -0400

    test

--- ais.py
+++ ais.py
@@ -51,6 +51,7 @@ class Model(object):
         #self.sess.run(init_op)
         self.sigma = sigma
         self.t = tf.placeholder(tf.float32, [], name='t')
+        print("OK here")
         self.lld = tf.reshape(-self.energy_fn(self.zv), [num_samples, self.batch_size, -1])
 
         self.stepsize = tf.Variable(stepsize)
